3809234798f6c40cea0742a78492378ade96aa20
professor434 <81698289+professor434@users.noreply.github.com>
2025-08-11 17:46:06 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
-// Import Buffer polyfill first before any other imports
-import './buffer-polyfill.ts';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import SolanaProviders from './providers/SolanaProviders';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import SolanaProviders from "./providers/SolanaProviders";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <SolanaProviders>
       <App />
     </SolanaProviders>
   </React.StrictMode>
 );
+
