c58be96a9c7e50376ac30ffff48db8fc80a12b0e
professor434 <81698289+professor434@users.noreply.github.com>
2025-08-11 19:33:34 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-// src/main.tsx
 import "./buffer-polyfill.ts";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 import "./index.css";
 import SolanaProviders from "./providers/SolanaProviders";
 
