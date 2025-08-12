6c61c78fac8af4a12281ae6fce8acb94b681818a
professor434 <81698289+professor434@users.noreply.github.com>
2025-08-12 12:17:33 +0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,16 +1,20 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string
-  readonly VITE_RPC_URL?: string
+  readonly VITE_APP_TITLE?: string
+  readonly VITE_API_BASE_URL?: string
+  readonly VITE_SOLANA_RPC_URL?: string
   readonly VITE_SOLANA_WS_URL?: string
-  // more env variables...
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
 
-interface Window {
-  lastTransactionSignature?: string;
+declare global {
+  interface Window {
+    lastTransactionSignature?: string
+    __CONF__?: any
+  }
 }
+export {}
