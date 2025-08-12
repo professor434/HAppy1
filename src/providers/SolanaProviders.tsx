0c169760ec234a234c203dbffb88ae5f5dc30eff
professor434 <81698289+professor434@users.noreply.github.com>
2025-08-12 23:16:20 +0300

    Update SolanaProviders.tsx

--- src/providers/SolanaProviders.tsx
+++ src/providers/SolanaProviders.tsx
@@ -2,8 +2,7 @@
 import React, { PropsWithChildren, useMemo } from "react";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
-import { SolflareWalletAdapter } from "@solana/wallet-adapter-solflare";
+import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
 import "@solana/wallet-adapter-react-ui/styles.css";
 import { makeConnection } from "@/lib/solana";
 
