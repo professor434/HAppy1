eabff9900a4ea5be187caf1f030b196cf8d9ccc0
professor434 <81698289+professor434@users.noreply.github.com>
2025-08-12 14:26:02 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import App from "./App.tsx";
 import "./index.css";
 import SolanaProviders from "./providers/SolanaProviders";
 
+import { assertEnv } from "@/lib/env";
+assertEnv();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <SolanaProviders>
