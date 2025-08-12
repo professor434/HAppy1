2292e7a228541a92eb5daf14411049bde9f121fe
professor434 <81698289+professor434@users.noreply.github.com>
2025-08-12 18:34:52 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
-// ΠΡΕΠΕΙ να είναι πρώτο
-import "./buffer-polyfill";
+// src/main.tsx
+import "./buffer-polyfill";   // πριν από Ο,ΤΙΔΗΠΟΤΕ (διορθώνει Buffer για libs)
+import "./index.css";         // global styles
+import "./App.css";           // app-level styles
 
 import React from "react";
 import ReactDOM from "react-dom/client";
