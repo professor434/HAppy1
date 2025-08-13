63463321fdcce5afc095a31579f540bef704f83e
professor434 <81698289+professor434@users.noreply.github.com>
2025-08-13 15:14:12 +0300

    Update buffer-polyfill.ts

--- src/buffer-polyfill.ts
+++ src/buffer-polyfill.ts
@@ -1,12 +1,5 @@
 // src/buffer-polyfill.ts
 import { Buffer } from "buffer";
-
-// Κάνε διαθέσιμο το Buffer παντού (browser)
-if (!(window as any).Buffer) {
+if (typeof window !== "undefined" && !(window as any).Buffer) {
   (window as any).Buffer = Buffer;
-}
-
-// (προαιρετικό) dummy process ώστε κάποια libs να μη σπάνε
-if (!(window as any).process) {
-  (window as any).process = { env: {} } as any;
 }
